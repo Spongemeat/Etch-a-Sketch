15dde111e0ecfafc882be2c6805e0dbeb0615483
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-04-17 10:55:39 -0400

    setup one row of squares for grid

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('testing script');
+const squareContainer = document.querySelector('.container');
+
+/* Change to alter grid dimensions, ex 16x16 grid*/
+let gridSize = 16;
+
+for (let i = 0; i < 16; i++){
+  let squareSetUp = document.createElement('div');
+  squareSetUp.classList.add('square');
+  squareContainer.appendChild(squareSetUp);
+}
+
+const squares = document.querySelectorAll('.square');
+
+for (let j = 0; j < squares.length; j++){
+  squares[j].style.backgroundColor = 'green';
+}
