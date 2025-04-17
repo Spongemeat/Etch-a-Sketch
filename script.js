00d0bdaaf2732b261bd8a7d1be69ff60086db957
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-04-17 11:51:49 -0400

    squares now turn grey when mouse enters square div elements, turns white when clicked

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const squareContainer = document.querySelector('.container');
 
-/* Change to alter grid dimensions, ex 16x16 grid*/
+// Change to alter grid dimensions, ex 16x16 grid //
 let gridSize = 16;
 for (let i = 0; i <16; i++){
   for (let j = 0; j < 16; j++){
@@ -12,13 +12,20 @@ for (let i = 0; i <16; i++){
 
 const squares = document.querySelector('.square');
 
+//if mouse enters element, square turns grey
 document.addEventListener('mouseover', function(event){
     if(event.target.classList.contains('square')){
-      console.log('hover works');
+      event.target.style.backgroundColor='grey';
     }
   }
 );
 
-/*for (let j = 0; j < squares.length; j++){
-  squares[j].style.backgroundColor = 'green';
-}*/
+//if mouse clicks square, square turns white
+document.addEventListener('click', function(event){
+    if(event.target.classList.contains('square')){
+      event.target.style.backgroundColor='white';
+    }
+  }
+);
+
+
