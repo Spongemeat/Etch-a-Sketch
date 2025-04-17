42a2ceb0b9a9013abdac63ecb12148f4281628ac
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-04-17 11:40:53 -0400

    implemented hover function test

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ for (let i = 0; i <16; i++){
   }
 };
 
-const squares = document.querySelectorAll('.square');
+const squares = document.querySelector('.square');
+
+document.addEventListener('mouseover', function(event){
+    if(event.target.classList.contains('square')){
+      console.log('hover works');
+    }
+  }
+);
 
 /*for (let j = 0; j < squares.length; j++){
   squares[j].style.backgroundColor = 'green';
