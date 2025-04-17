81173e09af76d2808bb5a9db917ac3d4dde0eb38
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-04-17 11:08:49 -0400

    script spits out 16x16 grid, edited style to center grid in browser

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const squareContainer = document.querySelector('.container');
 
 /* Change to alter grid dimensions, ex 16x16 grid*/
 let gridSize = 16;
-
-for (let i = 0; i < 16; i++){
-  let squareSetUp = document.createElement('div');
-  squareSetUp.classList.add('square');
-  squareContainer.appendChild(squareSetUp);
-}
+for (let i = 0; i <16; i++){
+  for (let j = 0; j < 16; j++){
+    let squareSetUp = document.createElement('div');
+    squareSetUp.classList.add('square');
+    squareContainer.appendChild(squareSetUp);
+  }
+};
 
 const squares = document.querySelectorAll('.square');
 
-for (let j = 0; j < squares.length; j++){
+/*for (let j = 0; j < squares.length; j++){
   squares[j].style.backgroundColor = 'green';
-}
+}*/
